afa3e761212422ddb9991e7f6c3edb25e52a0e43
kethirabalan <kethirabalantvl@gmail.com>
2024-05-18 18:04:47 +0530

    commit add

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { FooterComponent } from './footer/footer.component';
 import { NavbarComponent } from './navbar/navbar.component';
+import { EstimateComponent } from './estimate/estimate.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,FooterComponent,NavbarComponent],
+  imports: [RouterOutlet,FooterComponent,NavbarComponent,EstimateComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
