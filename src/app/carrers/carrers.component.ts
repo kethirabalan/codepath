1a372a561e616c4644f6f0eaa348f938714601c2
kethirabalan <kethirabalantvl@gmail.com>
2024-05-18 17:50:55 +0530

    first commit

---
+++ src/app/carrers/carrers.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-carrers',
+  standalone: true,
+  imports: [],
+  templateUrl: './carrers.component.html',
+  styleUrl: './carrers.component.scss'
+})
+export class CarrersComponent {
+
+}
---
+++ src/app/navbar/navbar.component.ts
@@ -0,0 +1,26 @@
+import { Component, HostListener } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-navbar',
+  standalone: true,
+  imports: [CommonModule, FormsModule],
+  templateUrl: './navbar.component.html',
+  styleUrls: ['./navbar.component.scss']
+})
+export class NavbarComponent {
+  isNavbarFixed: boolean = false;
+  isDarkMode: boolean = false;
+
+  @HostListener('window:scroll', ['$event'])
+  onWindowScroll() {
+    const offset = window.pageYOffset;
+    this.isNavbarFixed = offset > 50; // Adjust the value as needed
+  }
+
+  toggleDarkMode() {
+    this.isDarkMode = !this.isDarkMode;
+    document.body.classList.toggle('dark-mode', this.isDarkMode);
+  }
+}
