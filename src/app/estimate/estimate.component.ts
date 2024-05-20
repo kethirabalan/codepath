ee19d992cf93ca5c191984ea36055e9ac118fb55
kethirabalan <kethirabalantvl@gmail.com>
2024-05-20 18:42:00 +0530

    update

--- src/app/estimate/estimate.component.ts
+++ src/app/estimate/estimate.component.ts
@@ -3,11 +3,12 @@ import { NavbarComponent } from '../navbar/navbar.component';
 import { FooterComponent } from '../footer/footer.component';
 import { RouterLink, RouterOutlet } from '@angular/router';
 import { FormsModule } from '@angular/forms';
+import { ContactFormComponent } from '../contact-form/contact-form.component';
 
 @Component({
   selector: 'app-estimate',
   standalone: true,
-  imports: [RouterOutlet,RouterLink,FormsModule],
+  imports: [RouterOutlet,RouterLink,FormsModule,ContactFormComponent],
   templateUrl: './estimate.component.html',
   styleUrl: './estimate.component.scss'
 })
